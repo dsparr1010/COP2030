330e29973a051e1b0a91d4b616c52990d379964a
Debra <dsparr1010@gmail.com>
2021-03-06 10:45:37 -0600

    name readr part 2

--- 7_name_printer/name_printer.py
+++ 7_name_printer/name_printer.py
@@ -1,25 +1,14 @@
-"""
-Write a program to do the following:
+#3/6/2021
+#Debra Sparr
+#Assignment 7 - name printer
 
-Part 1:
-
-Creates a list with 6 names. The last name is to be used as a ‘sentinel’. Here is an example, but you should use your own names and sentinel
-Names = ['Alex', 'Brad', 'Jayne', 'Kim', 'Rafael', 'End']
-
-Implements a ‘while’ loop to display each name as long as the name is not ‘End’ since that is the sentinel
-Uses a try/except structure
-Part 2:
-
-Manually, not thru the program, please create a text file using mynames.txt as the name. (I will have a file with the same name to test your code. The program should read any number of names).
-In the text file, write several names, one per line.
-
-The program then uses ‘while’ loop to read and print the names in the text file, one line at a time
-"""
 
 NAMES = ["Aaran", "Aaren", "Aarez", "Aarman", "Aaron", "Aaron-James", "Aarron", "Aaryan", "Aaryn", "Azeem", "End"]
 NAMES2 = ["Strachan", "Struan", "Stuart", "Su", "Subhaan", "End", "Sudais", "Suheyb", "Suilven", "Sukhi", "Sukhpal", "Sukhvir"]
 NAMES_NO_SENTINEL = ["Aaran", "Aaren", "Aarez", "Aarman", "Aaron", "Aaron-James", "Aarron", "Aaryan", "Aaryn", "Azeem"]
 
+
+# PART 1
 def loop_to_sentinel(names_arr):
     """ Displays each name in the given list, as long as the name is not ‘End’ """
     SENTINEL = 'End'
@@ -38,7 +27,25 @@ def loop_to_sentinel(names_arr):
     except ValueError:
         print('You passed in a list without any names or did not provide the sentinel (End)!')
 
+# PART 2
+def name_reader(path):
+    """ Reads and prints the names in the text file.
 
-loop_to_sentinel(NAMES)
-loop_to_sentinel(NAMES2)
-loop_to_sentinel(NAMES_NO_SENTINEL)
+    Argument:
+    path : STRING, path to your text file
+    """
+    try:
+        with open(path, 'r') as reader:
+            line = reader.readline()
+            while line != '':  # Empty string signifies end of document
+                print(line, end='')
+                line = reader.readline()
+    except FileNotFoundError:
+        print("You need to pass in the path to your text file.")
+
+
+if __name__ == '__main__' :
+    loop_to_sentinel(NAMES)
+    loop_to_sentinel(NAMES2)
+    loop_to_sentinel(NAMES_NO_SENTINEL)
+    name_reader("") #use path to your text file, as a string, as the argument
