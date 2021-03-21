911ea2d34316f1b73201e0128291bfed8224e524
Debra <dsparr1010@gmail.com>
2021-03-21 13:44:06 -0500

    assingment 9 - OOP concepts

--- 9_shirt_sales/sale_class.py
+++ 9_shirt_sales/sale_class.py
@@ -8,17 +8,22 @@ class Sale:
         self.total = 0
 
     def __str__(self):
-        return f'A total number of {self.quantity} ordered with a grand total of {self.set_total}'
+        return f'A total number of {self.quantity} {self.item} ordered, with a grand total of ${self.total}'
 
     def set_total(self):
-        self.total = self.price*self.quantity
+        ''' A method to calculate the total '''
+        self.total = round(self.price*self.quantity, 2)
+        return
 
     def get_total(self):
+        ''' A method to return the total '''
         return f'The total is : {self.total}'
 
-    def get_quantitty(self):
+    def get_quantity(self):
+        ''' A method to return quantity'''
         return f'Total of {self.quantity} shirts'
 
     def get_item(self):
+        ''' A method to return the item '''
         return f'The item is {self.item}'
 
